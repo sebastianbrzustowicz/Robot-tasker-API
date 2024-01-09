2659263ae5342bc21e648613e518972f25bf70f9
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-01-10 00:25:05 +0100

    updating main app file location

--- src/main/java/pl/sebastianbrzustowicz/robotTaskerAPI/robotTaskerAPIApplication.java
+++ src/main/java/pl/sebastianbrzustowicz/robotTaskerAPI/robotTaskerAPIApplication.java
@@ -1,4 +1,4 @@
-package pl.sebastianbrzustowicz.robotTaskerAPI.application;
+package pl.sebastianbrzustowicz.robotTaskerAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
